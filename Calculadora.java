4cda161293c6c26ab13f153e55b55ce1416536ec
rodgolls <rodgolls@hotmail.com>
2020-03-10 03:34:15 -0300

    'subtração'

--- Calculadora.java
+++ Calculadora.java
@@ -6,8 +6,11 @@ public class Calculadora
       int a = Integer.parseInt(JOptionPane.showInputDialog("Digite um numero"));
       int b = Integer.parseInt(JOptionPane.showInputDialog("Digite um numero"));
       int res = a + b;
+      int res2 = a - b;
       String dois = "A soma do primeiro com o segundo é:" + " " + res + ".";
+      String tres = "A subtração do primeiro com o segundo é :" + " " + res2 + ".";
       JOptionPane.showMessageDialog(null,dois);
+      JOptionPane.showMessageDialog(null,tres);
       System.exit(0);
    }
 }
